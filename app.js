2ad22101cf166556f85683f1082bdd1502f611d8
RodrigoMontoya <rodrigomontoya1996@gmail.com>
2023-11-26 13:04:37 -0300

    falta terminar rutas y empece con el coontrolador

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const mainRoutes= require('./src/routes/mainRoutes');
+const shopRoutes= require('./src/routes/shopRoutes');
 
 app.use(express.static('public'))
 
 app.use('/',mainRoutes);
-
-app.get('/home',(req,res)=> res.sendFile(__dirname+'/public/index.html'));
+app.use('/',shopRoutes);
 
 app.listen(4000,()=>console.log("Servidor corriendo en http://localhost:4000"));
--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -2,5 +2,10 @@ const express= require('express');
 const router= express.Router();
 
 router.get('/shop',(req,res) =>res.send('Route for Shop View'));
+router.get('/shop/item/:id',(req,res) =>res.send('Route for find and retrieve a product from an ID'));
+router.post('/shop/item/:id/add',(req,res) =>res.send('Route for add  the current item  to the shop cart'));
+router.get('/shop/cart',(req,res) =>res.send('Route for Cart View'));
+router.post('/shop/cart',(req,res) =>res.send('Route for got to checkout page'));
+
 
 module.exports= router;
