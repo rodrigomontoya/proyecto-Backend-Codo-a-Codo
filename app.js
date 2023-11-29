42e2ec4f7f683e5a0404828b11a835c5aecc6f8d
RodrigoMontoya <rodrigomontoya1996@gmail.com>
2023-11-29 12:36:08 -0300

    realice primer render shop

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const methodOverride = require("method-override");
 const mainRoutes= require('./src/routes/mainRoutes');
 const shopRoutes= require('./src/routes/shopRoutes');
 const adminRoutes= require('./src/routes/adminRoutes');
@@ -12,6 +13,15 @@ app.use('/',mainRoutes);
 app.use('/',shopRoutes);
 app.use('/',adminRoutes);
 app.use('/',authRoutes);
+app.use(methodOverride("_method"));
+
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
+// esto se usa cuando esta mal la ruta de la pagina a la cual se quiere acceder
+app.use((req, res, next) => {
+    res.status(404).send("La pagina no existe");
+  });
 
 
 
--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,5 +1,5 @@
 const shopControllers={
-shop:(req,res) =>res.send('Route for Shop View'),
+shop:(req,res) =>res.render("shop"),
 shopItem:(req,res) =>res.send('Route for find and retrieve a product from an ID'),
 shopItemAdd:(req,res) =>res.send('Route for add  the current item  to the shop cart'),
 shopCartGet:(req,res) =>res.send('Route for Cart View'),
