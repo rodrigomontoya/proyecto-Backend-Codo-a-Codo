badbb93470962226be006d017abd6bd17ea19bd0
RodrigoMontoya <rodrigomontoya1996@gmail.com>
2023-12-08 12:14:47 -0300

    he terminado el multer en create

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,48 +1,42 @@
+const path = require("path");
+const sharp = require("sharp");
 
-  
-/* const adminControllers={
-admin:(req,res) =>res.render('admin/admin'),
-adminCreateGet:(req,res) =>res.render('admin/create'),
-adminCreatePost:(req,res) =>res.send('Producto Creado'),
-adminEditGet:(req,res) =>res.send('Route for read Item Admin'),
-adminEditPut:(req,res) =>res.send('Route for edit Item Admin'),
-adminDelete:(req,res) =>res.send('Route for  delete Item Admin'),
-}; */
-
-
-
-/* module.exports = {
-    adminControllers,
-  };  */
 
   const admin = (req, res) => {
     res.render("admin/admin");
   };
   
   const adminCreateGet = (req, res) => {
-    console.log(req.body);
     res.render('admin/create');
   };
   
   const adminCreatePost = (req, res) => {
-    console.log(req.params, req.body);
-    res.send("Producto modificado");
+    console.log( req.body,req.file);
+    // sharp se usa para renderizar la imagen y que mejore el funcionamiento de la pagina
+     //necesario el if para saber si estoy subiendo una imagen
+    if(req.file){
+        sharp(req.file.buffer)
+        .resize(300)
+        .toFile(path.resolve(__dirname,"../../public/uploads/producto1.jpg"))
+        .catch((err) =>console.log(err));
+    }
+
+        res.send('Producto Creado');  
+   
+   
   };
   
   const adminEditGet = (req, res) => {
     console.log(req.params);
-    res.send("Producto borrado");
-  };
-  const adminEditPost = (req, res) => {
-    console.log(req.params);
-    res.send("Producto borrado");
+    res.render('admin/edit');
   };
 
   // este es el update del profe
   const adminEditPut = (req, res) => {
-    console.log(req.params);
-    res.send("Producto borrado");
+    console.log(req.params,req.body);
+    res.send('Producto modificado');
   };
+
   const adminDelete = (req, res) => {
     console.log(req.params);
     res.send("Producto borrado");
