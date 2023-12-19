346fab1d50625dc142449e524a9f9f2cc7640532
RodrigoMontoya <rodrigomontoya1996@gmail.com>
2023-12-19 12:11:34 -0300

    enlas de los partials

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ app.use(
     saveUninitialized: false,
   })
 );
- const isLogin = (req, res, next) => {
-  if (req.session && req.session.user) {
+ /* const isLogin = (req, res, next) => {
+  if (!req.session.userId) {
     // El usuario está autenticado
     return next();
   } else {
     // Excluir rutas de inicio de sesión y otras rutas públicas
-    const publicPaths = ['/login', '/register', '/public','admin']; // Añade otras rutas públicas según sea necesario
+    const publicPaths = ['/login', '/register', '/public']; // Añade otras rutas públicas según sea necesario
     if (publicPaths.includes(req.path)) {
       return next();
     }
@@ -44,15 +44,38 @@ app.use(
     // El usuario no está autenticado, redirige al inicio de sesión
     res.redirect('/login');
   }
-};
+}; */
 
-/*  const isLogin = (req, res, next) => {
-  if (!req.session.userId) {
-    return res.redirect("/login");
+const isLogin = (req, res, next) => {
+  console.log(req.session);
+  const publicPaths = ['/login', '/register', '/public']; // Añade otras rutas públicas según sea necesario
+  if (publicPaths.includes(req.path)) {
+    // Si la ruta solicitada es pública, permite que la solicitud continúe
+    return next();
+  }
+  if (!req.session.userId ) {
+    // El usuario no está autenticado, redirige al inicio de sesión
+    return res.redirect('/login');
   }
 
+  // Excluir rutas de inicio de sesión y otras rutas públicas
+
+
+  // El usuario está autenticado, permite que la solicitud continúe
   next();
-}; */ 
+};
+
+
+ /*  const isLogin = (req, res, next) => {
+  if (!req.session.userId) {
+    return res.redirect("/login");
+  }else{
+
+    next();
+  }
+ */
+ 
+
 
 
 
